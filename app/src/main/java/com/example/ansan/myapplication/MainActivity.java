b3c657f7ab00610673dca98fa37f3db3499f37a2
LeeSeungHo <tmdgh6789@gmail.com>
2016-11-02 12:39:55 +0900

    폴더 생성 기능 추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.ansan.myapplication;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,9 +24,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) {
+        if (!isStoragePermissionGranted()) {
+            Toast.makeText(getApplicationContext(), "SD Card 사용 불가", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String path = Environment.getDownloadCacheDirectory().getAbsolutePath();
+        String folder = path + "/myLoveDir";
+        String fileName = folder + "/myFile.txt";
+
+        File myFolder = new File(folder);
+
         switch (v.getId()) {
             case R.id.button: // 폴더 삭제
-
+                myFolder.mkdir();
+                Toast.makeText(getApplicationContext(), "폴더 생성", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2: // 폴더 생성
 
