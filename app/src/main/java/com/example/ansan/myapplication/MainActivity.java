5a8f4636dda8a4dfd0a1c05739df21016a8fd5b4
LeeSeungHo <tmdgh6789@gmail.com>
2016-11-02 12:34:44 +0900

    SDCard 사용 권한 함수 추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.ansan.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +34,35 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button5: // 파일 목록 가져오기
 
                 break;
+        }
+    }
+
+    String TAG = "TEST";
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
         }
     }
 }
